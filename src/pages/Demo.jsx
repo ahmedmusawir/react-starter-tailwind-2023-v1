81d84121f4f40ca835c8b04727da6bb1b409648d
Ahmed Musawir <ahmed.musawir@gmail.com>
2023-01-27 10:21:36 +0800

    27Jan2023 - some fixes

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Box } from '../components/';
-import './Demo.scss';
+// import './Demo.scss';
 
 const Demo = () => {
   return (
@@ -33,8 +33,11 @@ const Demo = () => {
           Iure.
         </p>
       </Row>
-      <Row twClasses={'flex flex-wrap'}>
-        <Box>
+      {/* <Row twClasses={'flex flex-wrap justify-between mt-5'}> */}
+      <Row twClasses={'grid grid-auto-fit gap-5 my-5'}>
+        <Box twClasses={'min-w-min p-10 border'}>
+          {/* <Box twClasses={'xl:w-[750px] p-4 border m-2'}> */}
+          {/* <Box twClasses={'w-[48%] p-10 border '}> */}
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
             et, ex eum rem mollitia totam eius ad, sapiente eos maiores
@@ -42,7 +45,9 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box>
+        <Box twClasses={'min-w-min p-10 border'}>
+          {/* <Box twClasses={'xl:w-[750px] p-4 border m-2'}> */}
+          {/* <Box twClasses={'w-[48%] p-10 border '}> */}
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
             et, ex eum rem mollitia totam eius ad, sapiente eos maiores
@@ -51,8 +56,8 @@ const Demo = () => {
           </p>
         </Box>
       </Row>
-      <Row twClasses={'flex flex-wrap'}>
-        <Box twClasses={'w-80 p-5'}>
+      <Row twClasses={'flex flex-wrap justify-between'}>
+        <Box twClasses={'w-80 p-2 my-5 border'}>
           <img
             src="https://picsum.photos/id/12/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -65,7 +70,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'w-80 p-5'}>
+        <Box twClasses={'w-80 p-2 my-5 border'}>
           <img
             src="https://picsum.photos/id/16/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -78,7 +83,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'w-80 p-5'}>
+        <Box twClasses={'w-80 p-2 my-5 border'}>
           <img
             src="https://picsum.photos/id/15/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -95,7 +100,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'w-80 p-5'}>
+        <Box twClasses={'w-80 p-2 my-5 border'}>
           <img
             src="https://picsum.photos/id/61/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -151,10 +156,10 @@ const Demo = () => {
       </Row>
       <Row
         twClasses={
-          'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1'
+          'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'
         }
       >
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/62/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -167,7 +172,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/63/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -180,7 +185,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/64/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -193,7 +198,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/65/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -206,7 +211,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/66/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -219,7 +224,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-3 border shadow-lg'}>
           <img
             src="https://picsum.photos/id/67/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -236,8 +241,8 @@ const Demo = () => {
       <Row twClasses={'min-w-full text-center prose my-5'}>
         <h2>Tailwind Grid with Plugin</h2>
       </Row>
-      <Row twClasses={'grid gap-3 grid-auto-fit p-3'}>
-        <Box twClasses={'p-3'}>
+      <Row twClasses={'grid gap-5 grid-auto-fit p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/62/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -250,7 +255,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/63/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -263,7 +268,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/64/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -276,7 +281,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/65/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -289,7 +294,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/66/350/300"
             className="mb-3 min-w-full rounded-full"
@@ -302,7 +307,7 @@ const Demo = () => {
             quo. Iure.
           </p>
         </Box>
-        <Box twClasses={'p-3'}>
+        <Box twClasses={'p-10 border border-cyan-300 shadow-xl'}>
           <img
             src="https://picsum.photos/id/67/350/300"
             className="mb-3 min-w-full rounded-full"
--- src/pages/PageOne.jsx
+++ src/pages/PageOne.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
 import './PageOne.scss';
+
 import { Row } from '../components';
 function PageOne() {
   return (
     <>
-      <div className="moose-flex">
-        <div className="p-5 border-2">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-            et, ex eum rem mollitia totam eius ad, sapiente eos maiores
-            voluptatum, explicabo harum quos dolores nemo eaque reprehenderit
-            quo. Iure.
-          </p>
-        </div>
-
-        <div className="p-5 border-2">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-            et, ex eum rem mollitia totam eius ad, sapiente eos maiores
-            voluptatum, explicabo harum quos dolores nemo eaque reprehenderit
-            quo. Iure.
-          </p>
-        </div>
-
-        <div className="p-5 border-2">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-            et, ex eum rem mollitia totam eius ad, sapiente eos maiores
-            voluptatum, explicabo harum quos dolores nemo eaque reprehenderit
-            quo. Iure.
-          </p>
-        </div>
-
-        <div className="p-5 border-2">
+      <div className="prose">
+        <div className="p-7 border-2">
+          <h2>Tailwind Playground</h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
             et, ex eum rem mollitia totam eius ad, sapiente eos maiores
